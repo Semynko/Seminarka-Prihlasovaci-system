5d6830c662e5136ac47b41a0f11d85e94f428ed7
Semynko <semynko@protonmail.com>
2023-04-18 10:16:30 +0200

    t5

--- FormSystem.Designer.cs
+++ FormSystem.Designer.cs
@@ -29,13 +29,26 @@ namespace Autoskola
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(23, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 114);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Jízdy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormSystem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1083, 578);
+            this.ClientSize = new System.Drawing.Size(1083, 603);
+            this.Controls.Add(this.button1);
             this.Name = "FormSystem";
             this.Text = "FormSystem";
             this.ResumeLayout(false);
@@ -43,5 +56,7 @@ namespace Autoskola
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
